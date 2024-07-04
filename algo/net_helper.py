53efc0f12ed4ba305cd31ae973e3919b6eabd9c7
christian-werner <mail.christian-werner@web.de>
2024-07-05 01:16:09 +0200

    added accessibility and centrality examples

--- algo/net_helper.py
+++ algo/net_helper.py
@@ -34,3 +34,27 @@ def netascore_to_routable_net(netascore_gdf: gpd.GeoDataFrame, mode="bike", acce
     net.loc[~net[f"access_{access}_ft"], f"cost_{mode}_ft"] *= access_penalty_factor
     net.loc[(~net[f"access_{access}_ft"]) & (net[f"cost_{mode}_ft"].isna()), f"cost_{mode}_ft"] = 1000
     return net
+
+# helper function to add centrality results to input network DataFrame
+def add_centr_to_netascore(netascore_gdf: gpd.GeoDataFrame, centrality, centr_name: str, edge_key: str = "edge_id"):
+    # convert from dict with compound key to pandas df
+    tdf = pd.DataFrame(list(centrality.keys()))
+    tdf.rename(columns={0:"from_node", 1:"to_node", 2:"edge_key"}, inplace=True)
+    tdf["centrality"] = centrality.values()
+    # map centrality value back to original (geo)pandas df (for both directions)
+    if netascore_gdf.index.name == edge_key and edge_key in netascore_gdf.columns:
+        netascore_gdf.index.name = "index"
+    net_tmp = netascore_gdf.merge(tdf, left_on=["from_node", "to_node", edge_key], right_on=["from_node", "to_node", "edge_key"], how="left", suffixes=[None, "_b"])
+    net_tmp.rename(columns={"centrality":"centrality_ft"}, inplace=True)
+    net_ready = net_tmp.merge(tdf, left_on=["to_node", "from_node", "edge_key"], right_on=["from_node", "to_node", "edge_key"], how="left", suffixes=[None, "_c"])
+    net_ready.rename(columns={"centrality":"centrality_tf"}, inplace=True)
+    net_ready.set_index("edge_key", drop=False, inplace=True)
+    if edge_key in netascore_gdf.columns:
+        netascore_gdf.set_index(edge_key, drop=False, inplace=True)
+        netascore_gdf.index.name = "index"
+    if "index" in netascore_gdf.columns:
+        netascore_gdf.drop(columns=["index"], inplace=True)
+    netascore_gdf[f"centr_{centr_name}_ft"] = net_ready[~net_ready.index.isnull()].centrality_ft
+    netascore_gdf[f"centr_{centr_name}_tf"] = net_ready[~net_ready.index.isnull()].centrality_tf
+    centr_sum = net_ready.centrality_tf + net_ready.centrality_ft
+    netascore_gdf[f"centr_{centr_name}_sum"] = centr_sum[~centr_sum.index.isnull()]
